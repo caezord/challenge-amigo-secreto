d53961b234c99fee7793049e22d7c09e33191474
Caê <mcmlins17@gmail.com>
2025-01-23 14:49:07 -0300

    func sortearAmigo() implementada

--- app.js
+++ app.js
@@ -8,6 +8,31 @@ function adicionarAmigo(){
         alert("por favor insira um nome para ser sorteado!");
     }else{
         amigos.push(amigo);
+        atualizarLista();
         document.querySelector("#amigo").value = "";
     }
+}
+
+function atualizarLista() {
+    let listaAmigos = document.querySelector("#listaAmigos");
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        const amigo = amigos[i];
+        
+        const item = document.createElement("li");
+        
+        item.textContent = `${i + 1} - ${amigo}`;
+
+        listaAmigos.appendChild(item);
+    }
+    
+}
+
+function sortearAmigo(){
+    let resultado = document.querySelector("#resultado").value;
+
+    let escolhido = Math.floor(Math.random() * amigos.length);
+        resultado.innerHTML = `O Seu amigo(a) é o(a) ${(amigos[escolhido]).toUpperCase()}!`;
+        amigos.splice(escolhido, 1);
+        atualizarLista();
 }
