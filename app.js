aad628264878a29967743188bbfd896eb65b8e3b
Caê <mcmlins17@gmail.com>
2025-01-23 14:25:39 -0300

    func adicionarAmigo() implementada

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo(){
+    let amigo = document.querySelector("#amigo").value;
+    if(amigo == ""){
+        alert("por favor insira um nome para ser sorteado!");
+    }else{
+        amigos.push(amigo);
+        document.querySelector("#amigo").value = "";
+    }
+}
